fbc9c334a95d3fc494fab8609907ed62cb5ecf5c
liubin <liumeile0608@163.com>
2018-03-16 16:39:47 +0800

    Initial commit

---
+++ testdemo/locators.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-  
+'''
+__author__:liubin 
+
+'''
+
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+
+    GO_BUTTON = (By.ID, 'submit')
+
+
+class searchResultsPageLocators(object):
+    pass
